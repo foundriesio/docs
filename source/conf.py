b27dee04b9510c12f2c7c8088387e0207f5f51ef
Marti Bolivar <marti@opensourcefoundries.com>
2017-11-09 13:18:21 -0500

    Add fix for RTD table HTML output bug.
    The Read the Docs theme has a bug in how it generates tables, which
    prevents the text in the tables from wrapping in HTML output. The
    resulting tables can be very ugly and hard to read. Add a workaround,
    obtained from:
    
    https://rackerlabs.github.io/docs-rackspace/tools/rtd-tables.html
    
    Signed-off-by: Marti Bolivar <marti@opensourcefoundries.com>

--- source/conf.py
+++ source/conf.py
@@ -146,6 +146,15 @@ todo_include_todos = True
 # a list of builtin themes.
 html_theme = 'sphinx_rtd_theme'
 
+# RTD theme has a bug:
+# https://rackerlabs.github.io/docs-rackspace/tools/rtd-tables.html
+if html_theme == 'sphinx_rtd_theme':
+    html_context = {
+        'css_files': [
+            '_static/theme_overrides.css',
+        ],
+    }
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
