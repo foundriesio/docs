3ec656141f9cc374324e8966a30099a2f18c8271
Matthew Croughan <matthew.croughan@foundries.io>
2020-09-15 15:35:39 +0100

    index: update fio logo
    Signed-off-by: Matthew Croughan <matthew.croughan@foundries.io>
    Signed-off-by: Michael Scott <mike@foundries.io>

--- source/conf.py
+++ source/conf.py
@@ -221,7 +221,9 @@ html_theme = 'sphinx_rtd_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'logo_only': True
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
@@ -235,7 +237,7 @@ html_theme = 'sphinx_rtd_theme'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = "_static/logo.png"
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
