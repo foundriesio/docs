4efe1154552903fa4e277430575a00af4ebefbce
Matthew Croughan <matthew.croughan@foundries.io>
2020-11-27 17:59:42 +0000

    sphinx: add canonical_url for SEO #137
    Signed-off-by: Matthew Croughan <matthew.croughan@foundries.io>

--- source/conf.py
+++ source/conf.py
@@ -240,7 +240,8 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    'logo_only': True
+    'logo_only': True,
+    'canonical_url': 'https://docs.foundries.io/'
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
